d9177b6d442f4695e7a6b3fc427b9d2f4f00c450
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-06-10 20:32:35 -0300

    Sobre mim finalizado

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,5 +1,8 @@
 import "../../public/css/aboutme/style.css";
 import "../../public/css/responsive/media.css";
+import java from "../../public/img/java (1).png"
+import python from "../../public/img/python (1).png"
+import js from "../../public/img/js.png"
 
 export const AbouMeComponent = () => {
   const calculateAge = (birthDate) => {
@@ -23,46 +26,36 @@ export const AbouMeComponent = () => {
       <div className="card-left-about">
         <div className="sobremim c">
           <div className="title-card">
-            <h3>Sobre Mim</h3>
+            <h3 style={{textAlign: 'center'}}>Sobre Mim</h3>
           </div>
           <p style={{fontWeight: 'bold'}}>
-            Olá!! Me chamo Lucas Oliveira e tenho {age} anos. Estou atualmente no 3º semestre da faculdade de Ciências da Computação, com previsão de termino para 12/2027
-          </p>
-        </div>
-        <div className="objetivo c">
-          <div className="title-card">
-            <h3>Card Objetivo</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            numquam delectus nesciunt illum quae, nobis beatae consectetur
-            inventore, rem repellendus quas nihil distinctio quos dicta
-            adipisci. Similique modi nobis consequatur.
+            Olá! Meu nome é Lucas Oliveira e tenho {age} anos. Atualmente estou no 3º semestre do curso de Ciências da Computação, com previsão de conclusão em dezembro de 2027. Estou em busca de oportunidades para estágio ou uma posição como Desenvolvedor Júnior Back End. Sou uma pessoa dedicada e apaixonada por tecnologia, sempre buscando aprender e me desenvolver na área de desenvolvimento de software. 
+            <br/><br/>
+            Atualmente, estou focando meus estudos em conteúdos relacionados ao desenvolvimento Back End, incluindo microserviços, uso de design patterns, e outras tecnologias essenciais para a construção de sistemas escaláveis e eficientes.
           </p>
         </div>
       </div>
-      <div className="card-right-about">
-        <div className="projetos c">
-          <div className="title-card">
-            <h3>Card Projetos</h3>
-          </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
-            numquam delectus nesciunt illum quae, nobis beatae consectetur
-            inventore, rem repellendus quas nihil distinctio quos dicta
-            adipisci. Similique modi nobis consequatur.
+      <div className="card-right-about">    
+        <div className="skills c"
+          style={{
+
+            height: "10%"
+          }}
+        >
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <p style={{
+            display: "flex", gap: "10px"
+          }}>
+            <img src={java} alt="" width={"70px"} height={"70px"} className="skill-icon"/>
+            <img src={python} alt="" width={"70px"} height={"70px"} className="skill-icon"/>
+            <img src={js} alt="" width={"70px"} height={"70px"} className="skill-icon"/>
           </p>
-        </div>
-        <div className="skills c">
-          <div className="title-card">
-            <h3>Card skills</h3>
           </div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum
-            minima possimus soluta minus! Dolorem rerum nam ad autem dolor
-            assumenda exercitationem, temporibus placeat, praesentium qui quia
-            magni, nemo ullam iure!
-          </p>
+          
         </div>
       </div>
     </div>
