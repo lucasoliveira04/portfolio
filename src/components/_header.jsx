c0556448c311a0bb7357522dc04d3ffd1ed3e0f9
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-02-15 18:42:09 -0300

    feat : header estilizada

--- src/components/_header.jsx
+++ src/components/_header.jsx
@@ -1,22 +1,51 @@
-import "../../public/css/header.css";
+import { useState } from "react";
 
-export const HeaderComponent = () => {
+function HeaderComponent({ setLanguage }) {
+    const texts = {
+        pt: {
+            projects: "Projetos",
+            aboutMe: "Sobre Mim",
+            contactsMe: "Entre em contato",
+        },
+        en: {
+            projects: "Projects",
+            aboutMe: "About Me",
+            contactsMe: "Contact Me"
+        },
+    };
+
+    const [isEnglish, setIsEnglish] = useState(() => {
+        return localStorage.getItem("language") === "en";
+    });
+
+    const toggleLanguage = () => {
+        const newLanguage = isEnglish ? "pt" : "en";
+        localStorage.setItem("language", newLanguage);
+        setIsEnglish(!isEnglish);
+        setLanguage(newLanguage);
+    };
 
     return (
-        <div className={`container-header`}>
-            <div>
-                <h2 id={"#container-header"}>Lucas Oliveira Campos</h2>
+        <div className="w-full p-[23px] bg-[#111316] flex justify-between items-center fixed">
+            <div className="flex gap-3 ">
+                <p className="font-sans cursor-pointer text-gray-400 hover:text-white transition-colors duration-300">{texts[isEnglish ? "en" : "pt"].projects}</p>
+                <p className="font-sans cursor-pointer text-gray-400 hover:text-white transition-colors duration-300">{texts[isEnglish ? "en" : "pt"].aboutMe}</p>
+                <p className="font-sans cursor-pointer text-gray-400 hover:text-white transition-colors duration-300">{texts[isEnglish ? "en" : "pt"].contactsMe}</p>
             </div>
-
-            <div>
-                <nav>
-                    <ul>
-                        <li>Projetos</li>
-                        <li>Sobre Mim</li>
-                        <li>Contatos</li>
-                    </ul>
-                </nav>
-            </div>
+            <button
+                onClick={toggleLanguage}
+                className="text-white text-2xl transform transition-transform duration-200 ease-in-out hover:scale-110"
+            >
+                <img
+                    src={isEnglish ? "/img/eua.png" : "/img/brasil.png"}
+                    alt="Bandeira"
+                    width={36}
+                    height={24}
+                    className="rounded shadow-lg"
+                />
+            </button>
         </div>
     );
-};
+}
+
+export default HeaderComponent;
