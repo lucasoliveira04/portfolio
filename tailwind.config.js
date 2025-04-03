6ec425b14ed189b1b7f45a54955a72d63b69153f
= <camposdlucasoli@gmail.com>
2025-04-03 19:36:40 -0300

    add imagens de Skills

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,24 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      screens:{
+        xs: {max: "630px"}
+      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
-        sigma: ['Sigmar', 'sans-serif']
-      }
+        sigma: ['Sigmar', 'sans-serif'],
+        boldonse: ['Boldonse', 'sans-serif'],
+
+      },
+      animation: {
+        float: "float 2s ease-in-out infinite",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-5px)" },
+        },
+      },
     },
   },
   plugins: [],
