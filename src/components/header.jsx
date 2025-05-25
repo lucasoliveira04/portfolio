a4d37d4479d42287999fcce8de76b8271f11324f
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-25 10:31:27 -0300

    Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,22 +2,13 @@ import { useTranslation } from "react-i18next";
 import { HamburgerMenu } from "./hamburgerMenu.jsx";
 import brazil from "../../public/img/countrys/square.png";
 import eua from "../../public/img/countrys/united-states.png";
-import { useNavigate, useParams } from "react-router";
+import {useEffect, useState} from "react";
+import {useLanguageToggle} from "../hook/useLanguageToggle.js";
 
 export function HeaderComponent() {
-    const { t, i18n } = useTranslation();
-    const { lang } = useParams();
-    const navigate = useNavigate();
-
-    const toggleLanguage = () => {
-        const newLang = lang === "pt" ? "en" : "pt";
-
-        i18n.changeLanguage(newLang);
-
-        const currentPath = window.location.pathname;
-        const updatePath = currentPath.replace(`/${lang}`, `/${newLang}`);
-        navigate(updatePath);
-    };
+    const { t } = useTranslation();
+    const { lang, toggleLanguage } = useLanguageToggle();
+    const [showBorder, setShowBorder] = useState(false);
 
     const navOptions = [
         { key: "aboutMe", label: t("header.navigation.aboutMe") },
@@ -26,15 +17,43 @@ export function HeaderComponent() {
         { key: "contact", label: t("header.navigation.contact") },
     ];
 
-    return (
-        <header className="fixed flex w-full justify-between items-center p-4 bg-gradient-to-r from-white to-green-100 shadow-md">
-            <div>
-                <h5 className="text-3xl font-extrabold font-inter text-gray-900">
-                    Lucas Oliveira
-                </h5>
-            </div>
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 100) {
+                setShowBorder(true);
+            } else {
+                setShowBorder(false);
+            }
+        };
 
-            <div className="flex items-center gap-6">
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    return (
+        <header
+            className={`fixed flex w-full justify-between items-center p-4 bg-gradient-to-r from-white to-green-100 transition-all duration-300 ${
+                showBorder ? "border-b border-gray-300" : "border-b-0"
+            }`}
+        >
+            <div className="flex items-center w-full gap-6">
+
+                <ul className="flex gap-5 w-full justify-center">
+                    {navOptions.map((navOption) => (
+                        <li key={navOption.key}>
+                            <button
+                                className="text-gray-800 hover:text-green-600 transition font-fredoka text-[19px]"
+                                onClick={() => {
+                                    const section = document.getElementById(navOption.key);
+                                    section?.scrollIntoView({ behavior: "smooth" });
+                                }}
+                            >
+                                {navOption.label}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+
                 <nav className="block md:hidden">
                     <HamburgerMenu navOptions={navOptions} />
                 </nav>
@@ -50,3 +69,4 @@ export function HeaderComponent() {
         </header>
     );
 }
+
