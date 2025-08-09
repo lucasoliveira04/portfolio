9472d7cd7b5bb8394b0b26bb10f59fd18744260a
= <lucasolisocialmedia@gmail.com>
2025-08-09 18:21:17 -0300

    criando estrutura para separar as renposabilidades de um script e separando os projetos em sub classes

---
+++ src/model/base/projectCardBase.js
@@ -0,0 +1,27 @@
+export class ProjectCardBase {
+  constructor(
+    viewProject,
+    title,
+    description,
+    langs,
+    github,
+    repoUrl,
+    visitUrl,
+    visit,
+    startDate,
+    images,
+    tags
+  ) {
+    this.viewProject = viewProject;
+    this.title = title;
+    this.description = description;
+    this.langs = langs;
+    this.github = github;
+    this.repoUrl = repoUrl;
+    this.visitUrl = visitUrl;
+    this.visit = visit;
+    this.startDate = startDate;
+    this.images = images;
+    this.tags = tags;
+  }
+}
---
+++ src/model/subclasses/processImageAPI.js
@@ -0,0 +1,33 @@
+import { ProjectCardBase } from "../base/projectCardBase";
+
+import python from "../../assets/langs/python.png";
+import flask from "../../assets/langs/Flask.png";
+import redis from "../../assets/langs/redis.png";
+import docker from "../../assets/langs/docker.png";
+import rabbitmq from "../../assets/langs/RabbitMQ.png";
+
+import estagio from "../../assets/projects/estagio.png";
+
+export class ProcessImageApi extends ProjectCardBase {
+  constructor(t) {
+    super(
+      true,
+      t("projectsAndExperiences.processImageAPI.title"),
+      t("projectsAndExperiences.processImageAPI.description"),
+      [
+        { name: "Python", icon: python },
+        { name: "Flask", icon: flask },
+        { name: "Redis", icon: redis },
+        { name: "RabbitMQ", icon: rabbitmq },
+        { name: "Docker", icon: docker },
+      ],
+      "Github",
+      "https://github.com/lucasoliveira04/api-process-image.git",
+      null,
+      null,
+      "Jun 2025",
+      [estagio],
+      ["backend"]
+    );
+  }
+}
