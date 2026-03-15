af602cf36d0b3bda8245f92fcfbb36b9219ad4a2
lucasoliveira04 <lucasolisocialmedia@gmail.com>
2026-03-15 17:01:33 -0300

    feat: initialize Spring Boot project with Maven setup and basic application structure

---
+++ src/main/java/com/portfolio_web/portfolio_prod/PortfolioProdApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio_web.portfolio_prod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioProdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio_web/portfolio_prod/PortfolioProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio_web.portfolio_prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
