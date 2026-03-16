9b5754ec6ce9568296d0bf1b20e5f8dd8bd2808d
lucasoliveira04 <lucasolisocialmedia@gmail.com>
2026-03-16 18:25:21 -0300

    docs: added todo comment in about-me component

--- src/app/components/about-me/about-me.ts
+++ src/app/components/about-me/about-me.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { INTERESTS, SKILLS, TIMELINE } from '../../constants/about.constants';
-
+// TODO: move constants to a service and fetch them from there
 @Component({
   selector: 'app-about-me',
   imports: [TranslateModule],
